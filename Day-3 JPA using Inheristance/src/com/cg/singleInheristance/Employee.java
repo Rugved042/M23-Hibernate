b645ca84d29782f407c0a286a2a75fb8dc2b15dc
Rugved <Rugved@DESKTOP-3ETN7D0>
2022-09-17 16:48:54 +0530

    first commit

---
+++ Day-3 JPA using Inheristance/src/com/cg/singleInheristance/Employee.java
@@ -0,0 +1,54 @@
+package com.cg.singleInheristance;
+
+import java.io.Serializable;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+//this code is not giving result
+@Entity
+@Table(name = "Employee2")
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "emp_type,discriminatorType = DiscriminatorType.STRING")
+@DiscriminatorValue("emp")
+public class Employee implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	//it creates ID automacally in MySlq
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int EmpId;
+	private String name;
+	private float salary;
+	
+	//Getters and setters Method
+	public int getEmpId() {
+		return EmpId;
+	}
+	public void setEmpId(int empId) {
+		EmpId = empId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	
+	
+
+}
---
+++ Day-3 JPA using Inheristance/src/com/cg/tableperclassinheritance/Client.java
@@ -0,0 +1,40 @@
+package com.cg.tableperclassinheritance;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Client {
+
+	public static void main(String[] args) 
+	{
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		
+		//create an employee
+		Employee e = new Employee();
+		e.setName("Rugved");
+		e.setSalary(35000);
+		em.persist(e);
+		
+		//create an employee 1
+		Employee e1  = new Employee();
+		e1.setName("Akshay");
+		e1.setSalary(40000);
+		em.persist(e1);
+		
+		//create a manager
+		Manager m = new Manager();
+		m.setName("Guru");
+		m.setSalary(60000);
+		m.setDeptname("Information Technology");
+		em.persist(m);
+		
+		em.getTransaction().commit();
+		System.out.println("date is inserted");
+		em.close();
+		factory.close();
+	}
+
+}
